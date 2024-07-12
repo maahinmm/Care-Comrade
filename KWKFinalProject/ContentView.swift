8115878cb1da0d93259723f6971cbae5dd5fd865
maahinmm <maahinmirza@gmail.com>
2024-07-12 16:07:46 -0400

    Initial Commit

---
+++ KWKFinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KWKFinalProject
+//
+//  Created by Maahin Mirza on 7/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KWKFinalProject/KWKFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  KWKFinalProjectApp.swift
+//  KWKFinalProject
+//
+//  Created by Maahin Mirza on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KWKFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
