1471939eff77e2eea893c3abe1ba3c2f0883fba6
maahinmm <maahinmirza@gmail.com>
2024-07-17 17:49:36 -0400

    new day 8 work

--- KWKFinalProject/ContentView.swift
+++ KWKFinalProject/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, adkjsdj!")
+  var body: some View {
+    ZStack {
+      Color(red: 0.945, green: 0.945, blue: 0.945)
+        .ignoresSafeArea()
+      VStack (alignment: .leading, spacing: 20.0) {
+        Image("logo")
+          .resizable(resizingMode: .stretch)
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(15)
+          Text("Joy Buolamwini")
+            .font(.title)
+            .fontWeight(.bold)
+          Spacer()
+          Text("Poet of Code")
         }
+        Text("Founder of the Algorithmic Justice League. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology")
+      .padding()
+      .background(Rectangle().foregroundColor(Color(red: 0.945, green: 0.945, blue: 0.945)).cornerRadius(20.0))
+      .padding()
     }
+  }
 }
 #Preview {
-    ContentView()
+  ContentView()
 }
