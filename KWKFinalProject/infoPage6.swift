03cf4ab8419f6f71f7452d18380340295fdee1d3
Akreti <akretisharma06@gmail.com>
2024-07-19 14:00:08 -0400

    changes to help pages

---
+++ KWKFinalProject/infoPage6.swift
@@ -0,0 +1,18 @@
+//
+//  infoPage6.swift
+//  KWKFinalProject
+//
+//  Created by Akreti Sharma on 2024-07-19.
+//
+
+import SwiftUI
+
+struct infoPage6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    infoPage6()
+}
